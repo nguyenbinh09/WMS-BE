e9c4f8240d4b41295c32f89f1ae4a642d67d5d57
nguyenbinh09 <21521878@gm.uit.edu.vn>
2024-01-10 21:48:33 +0700

    get transaction by warehouse id

--- src/routes/warehouseRoute.js
+++ src/routes/warehouseRoute.js
@@ -3,11 +3,7 @@ const warehouseController = require("../controllers/warehouseController");
 
 const router = require("express").Router();
 
-router.post(
-  "/",
-
-  warehouseController.addWarehouse
-);
+router.post("/", warehouseController.addWarehouse);
 router.get("/", warehouseController.getAllWarehouse);
 router.get("/:id", warehouseController.getWarehouse);
 router.put("/:id", warehouseController.updateWarehouse);
